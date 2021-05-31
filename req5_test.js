1f1affdd28e9d8ff137aa04182c0f3824eb9e841
Tetiana12345A <85074274+Tetiana12345A@users.noreply.github.com>
2021-05-31 12:08:36 +0300

    Update req5_test.js

--- req5_test.js
+++ req5_test.js
@@ -20,7 +20,6 @@ const compare = async (I, tagName) => {
     I.seeNumberOfVisibleElements('$influencer-name', data.length);
     const influencers = await I.grabTextFromAll('$influencer-name');
 
-
     const hasTagsAndEqual = data.every((item, i) => {
         const hasTag = item.item.tags.some((tag) => tag.name === tagName);
         return hasTag && (item.performer.displayName === influencers[i]);
@@ -38,7 +37,6 @@ Scenario('check phisics match topics tarot', async ({ I }) => {
     const areEqual = await compare(I,  'Tarot');
     assert.strictEqual(areEqual, true);
 });
-
 
 Scenario('check phisics match topics dreams', async ({ I }) => {
     const areEqual = await compare(I,  'Dreams');
