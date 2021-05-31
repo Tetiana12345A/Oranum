52895a2202837ebb17e47c86a06d7a8067fbfa30
Tetiana12345A <85074274+Tetiana12345A@users.noreply.github.com>
2021-05-31 10:29:30 +0300

    Update req1_test.js

--- req1_test.js
+++ req1_test.js
@@ -1,4 +1,4 @@
-//// did not find Show More button , so check duplicates for new psychics when scroll 
+//did not find Show More button , so check duplicates for new psychics when scroll 
 
 
 const assert = require('assert');
